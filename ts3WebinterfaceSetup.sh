15d6b8483358bac5ccdd30bada47d9f184b96140
Syndesi <soerenklein98@gmail.com>
2017-09-30 13:55:35 +0200

    changed ts3

--- ts3WebinterfaceSetup.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-TMPFILE           = "tmp.zip"
-PATH              = "/var/www/html/ts3/"
-TS3_WEB_INTERFACE = "https://github.com/Syndesi/Snippet/raw/master/ts3wi_3.4.3.1.zip"
-
-# Download and extract the TS3-Webinterface
-wget $TS3_WEB_INTERFACE -O $TMPFILE
-unzip -d $PATH $TMPFILE
-rm $TMPFILE
